e4515be812908de7d79d29abe845f0fc9771f090
artur-hurynovich <artur.hurynovich@gmail.com>
2019-01-09 15:44:21 +0300

    fixed validation issue

--- src/main/java/com/gpsolutions/vaadincourses/form/EmailForm.java
+++ src/main/java/com/gpsolutions/vaadincourses/form/EmailForm.java
@@ -8,6 +8,7 @@ import com.vaadin.data.Validator;
 import com.vaadin.data.fieldgroup.BeanFieldGroup;
 import com.vaadin.data.fieldgroup.FieldGroup;
 import com.vaadin.data.fieldgroup.PropertyId;
+import com.vaadin.ui.AbstractField;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification;
@@ -18,6 +19,8 @@ import com.vaadin.ui.VerticalLayout;
 import org.springframework.beans.BeanUtils;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class EmailForm extends Panel {
@@ -38,36 +41,23 @@ public class EmailForm extends Panel {
 
     private final BeanFieldGroup<EmailEntity> emailFieldGroup = new BeanFieldGroup<>(EmailEntity.class);
 
-    @SuppressWarnings("unchecked")
+    private final List<AbstractField> fields;
+
     public EmailForm(final EmailEntity emailEntity, final EmailRepository emailRepository,
                      final Runnable onSave, final Runnable onDiscard) {
         this.emailRepository = emailRepository;
         nameField.setNullRepresentation("");
+        nameField.addTextChangeListener(textChangeEvent -> nameField.setValue(textChangeEvent.getText()));
         textField.setNullRepresentation("");
+        textField.addTextChangeListener(textChangeEvent -> textField.setValue(textChangeEvent.getText()));
         final VerticalLayout layout = new VerticalLayout();
         final HorizontalLayout buttonsLayout = new HorizontalLayout();
         buttonsLayout.addComponents(getSaveButton(emailEntity, onSave), getCancelButton(onDiscard));
         emailFieldGroup.setItemDataSource(emailEntity);
         emailFieldGroup.bindMemberFields(this);
-        dateField.addValidator(value -> {
-            if (value == null) {
-                throw new Validator.InvalidValueException("The date is empty");
-            }
-            if (((LocalDate) value).isBefore(LocalDate.now())) {
-                throw new Validator.InvalidValueException("The date is before today");
-            }
-        });
-        recipientsField.addValidator(value -> {
-            final List<String> recipients = (List<String>) value;
-            if (recipients.size() > 2) {
-                throw new Validator.InvalidValueException("There should be a maximum of 2 recipients");
-            }
-            for (String recipient : (recipients)) {
-                if (recipient.isEmpty()) {
-                    throw new Validator.InvalidValueException("There should be no recipients without a name");
-                }
-            }
-        });
+        fields = new ArrayList<>();
+        fields.addAll(Arrays.asList(nameField, textField, dateField, recipientsField));
+        setValidators();
         layout.addComponents(nameField, textField, recipientsField, dateField, buttonsLayout);
         setContent(layout);
     }
@@ -76,6 +66,7 @@ public class EmailForm extends Panel {
         final Button saveButton = new Button("Save");
         saveButton.addClickListener(clickEvent -> {
             try {
+                setValidatorsVisibility();
                 emailFieldGroup.commit();
                 final EmailEntity newEmail = emailFieldGroup.getItemDataSource().getBean();
                 BeanUtils.copyProperties(newEmail, oldEmailEntity);
@@ -96,6 +87,50 @@ public class EmailForm extends Panel {
             onDiscard.run();
         });
         return cancelButton;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void setValidators() {
+        nameField.addValidator(value -> {
+            if (nameField.getValue() == null || nameField.getValue().length() < 1 || nameField.getValue().length() > 10) {
+                throw new Validator.InvalidValueException("The name length should be between 1 and 10 characters");
+            }
+        });
+        textField.addValidator(value -> {
+            if (textField.getValue() == null || textField.getValue().length() < 1 || textField.getValue().length() > 30) {
+                throw new Validator.InvalidValueException("The message length should be between 1 and 30 characters");
+            }
+        });
+        dateField.addValidator(value -> {
+            if (value == null) {
+                throw new Validator.InvalidValueException("The date is empty");
+            }
+            if (((LocalDate) value).isBefore(LocalDate.now())) {
+                throw new Validator.InvalidValueException("The date is before today");
+            }
+        });
+        recipientsField.addValidator(value -> {
+            final List<String> recipients = (List<String>) value;
+            if (recipients.size() > 2) {
+                throw new Validator.InvalidValueException("There should be a maximum of 2 recipients");
+            }
+            for (String recipient : (recipients)) {
+                if (recipient.isEmpty()) {
+                    throw new Validator.InvalidValueException("There should be no recipients without a name");
+                }
+            }
+        });
+        fields.forEach(field -> field.setValidationVisible(false));
+    }
+
+    private void setValidatorsVisibility() {
+        fields.forEach(field -> {
+            if (field.isValid()) {
+                field.setValidationVisible(false);
+            } else {
+                field.setValidationVisible(true);
+            }
+        });
     }
 
 }
